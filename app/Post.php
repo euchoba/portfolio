270231dd0c321c03043c4cd1255763e4abddc5a9
euchoba <euchoba@icloud.com>
2018-04-17 09:04:39 +0200

    Add Comments

--- app/Post.php
+++ app/Post.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use function compact;
+use Illuminate\Database\Eloquent\Model;
+
 class Post extends Model
 {
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+        $this->comments()->create(compact('body'));
     }
 }
