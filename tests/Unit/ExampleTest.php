afa7c693a5e4f695b176c9ca7f0f6ce7242e8069
euchoba <euchoba@icloud.com>
2018-04-18 09:45:42 +0200

    Testing 101

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use Carbon\Carbon;
+use function dd;
+use function factory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,6 +19,28 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        // (Given) I have two records in the database that are posts,
+        // and each one is posted a month apart.
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+            'created_at' => Carbon::now()->subMonth()
+        ]);
+
+        // (When) I fetch the archives.
+        $posts = Post::archives();
+
+        // (Then) the response should be in the proper format.
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+            ],
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+            ]
+        ], $posts);
     }
 }
